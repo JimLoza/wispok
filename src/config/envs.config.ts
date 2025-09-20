69083c9c8f1dd98bfdd0b4200fb6b05b1f444aa3
JimLoza <jimloza25@gmail.com>
2025-09-20 14:34:01 -0600

    feat(config): Initial setup envs

---
+++ src/config/envs.config.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import * as Joi from "joi";
+
+interface EnvVars {
+    PORT: number;
+    DB_URI: string;
+}
+
+export const envsSchema = Joi.object<EnvVars>({
+    PORT: Joi.number().required().default(3000),
+    DB_URI: Joi.string().required(),
+}).unknown(true);
+
+
+const { error, value } = envsSchema.validate(process.env);
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+export const envVars: EnvVars = value as EnvVars;
+
+export const envs = {
+    port: envVars.PORT,
+    database: {
+        uri: envVars.DB_URI,
+    }
+}
+

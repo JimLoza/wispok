452f075e9ef6f1d4cb8d7a25f9a81a09715c1384
JimLoza <jimloza25@gmail.com>
2025-09-24 03:55:49 -0600

    feat(app): add basic security middlewares to the app

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,26 @@ import express, { Request, Response } from "express";
 import "dotenv/config";
 import dbConnection from './config/database/mongo.config';
 import router from './components/index.routes';
+import cors from 'cors';
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos en milisegundos
+  max: 100,
+  message: 'Demasiadas peticiones desde esta IP, por favor intente de nuevo después de 15 minutos.'
+});
+
 app.use(express.json());
+app.use(cors({
+  origin: '*',
+}))
+app.use(helmet())
+app.use(apiLimiter);
+
+
 dbConnection().then(() => console.log("Database connected"));
 
 
